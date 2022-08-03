c08913f673eff39ea381e22c6d042792114c905a
borhenn005 <borhen.fatnassi@esprit.tn>
2022-08-03 17:25:57 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/escaperoom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.escaperoom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Escape VRoom',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(body: Text('Escape VRoom')),
+    );
+  }
+}
